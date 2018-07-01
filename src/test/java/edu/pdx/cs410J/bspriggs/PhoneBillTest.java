da080895b9d4ae215c738595f2707a0bbc4f5a2b
bspriggs <ben@sprico.com>
2018-07-01 16:58:28 -0700

    Add PhoneBill test and class

---
+++ src/test/java/edu/pdx/cs410J/bspriggs/PhoneBillTest.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.bspriggs;
+
+import org.junit.Test;
+
+public class PhoneBillTest {
+
+    @Test
+    public void getCustomer() {
+    }
+
+    @Test
+    public void addPhoneCall() {
+    }
+
+    @Test
+    public void getPhoneCalls() {
+    }
+}
