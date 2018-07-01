d74c6660f7871a3543d6b70afec93fb62f377b0a
bspriggs <ben@sprico.com>
2018-07-01 16:54:50 -0700

    Use list to preserve arg ordering

--- src/main/java/edu/pdx/cs410J/bspriggs/Project1.java
+++ src/main/java/edu/pdx/cs410J/bspriggs/Project1.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs410J.bspriggs;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import static java.util.Map.entry;
@@ -8,28 +10,25 @@ import static java.util.Map.entry;
  * The main class for the CS410J Phone Bill Project
  */
 public class Project1 {
-  private static final Map<String, String> ARGUMENTS = Map.ofEntries(
+  private static final List<Map.Entry<String, String>> ARGUMENTS = Arrays.asList(
           entry("customer", "Person whose phone bill we’re modeling"),
           entry("callerNumber", "Phone number of caller"),
-          entry("calleeNumber", " Phone number of person who was called"),
-          entry("startTime", " Date and time call began (24-hour time)"),
-          entry("endTime", " Date and time call ended (24-hour time)")
+          entry("calleeNumber", "Phone number of person who was called"),
+          entry("startTime", "Date and time call began (24-hour time)"),
+          entry("endTime", "Date and time call ended (24-hour time)")
   );
 
-  private static final Map<String, String> OPTIONS = Map.ofEntries(
+  private static final List<Map.Entry<String, String>> OPTIONS = Arrays.asList(
           entry("-print", "Prints a description of the new phone call"),
           entry("-README", "Prints a README for this project and exits")
   );
 
-  private static String build(Map<String, String> f) {
+  private static String build(List<Map.Entry<String, String>> f) {
     var b = new StringBuilder();
 
-    for (Map.Entry<String, String> pair : f.entrySet()) {
-      b.append("  ");
-      b.append(pair.getKey());
-        b.append("\t\t\t");
-      b.append(pair.getValue());
-      b.append('\n');
+    for (Map.Entry<String, String> pair : f) {
+      b.append(String.format("  %-10s\t%s", pair.getKey(), pair.getValue()));
+      b.append(System.lineSeparator());
     }
 
     return b.toString();
