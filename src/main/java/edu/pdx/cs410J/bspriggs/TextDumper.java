dd82d412b7a095f1014817eefd01f17dd7a79485
bspriggs <ben@sprico.com>
2018-07-12 11:26:50 -0700

    Add constructors and methods to make tests compile

--- src/main/java/edu/pdx/cs410J/bspriggs/TextDumper.java
+++ src/main/java/edu/pdx/cs410J/bspriggs/TextDumper.java
@@ -2,7 +2,23 @@ package edu.pdx.cs410J.bspriggs;
 
 import edu.pdx.cs410J.AbstractPhoneBill;
 
+import java.io.DataOutputStream;
+
 public class TextDumper implements edu.pdx.cs410J.PhoneBillDumper {
+    private final String fileName;
+
+    public TextDumper(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public TextDumper() {
+        this.fileName = "";
+    }
+
+    public void dumpTo(AbstractPhoneBill phoneBill, DataOutputStream dataOutputStream) {
+
+    }
+
     @Override
     public void dump(AbstractPhoneBill abstractPhoneBill) {
 
