6067b9e15ca2d438e3da56c10447caadf035f784
bspriggs <ben@sprico.com>
2018-07-12 12:12:41 -0700

    Flesh out more file-based testing

--- src/test/java/edu/pdx/cs410J/bspriggs/TextDumperTest.java
+++ src/test/java/edu/pdx/cs410J/bspriggs/TextDumperTest.java
@@ -1,19 +1,30 @@
 package edu.pdx.cs410J.bspriggs;
 
 import edu.pdx.cs410J.AbstractPhoneCall;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class TextDumperTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     /**
-     * Tests that dumping an empty phone bill returns nothing
+     * Tests that dumping an empty phone bill returns nothing.
      */
     @Test
     public void testDumpToEmptyPhoneBill() {
@@ -26,7 +37,7 @@ public class TextDumperTest {
     }
 
     /**
-     * Tests that dumping a phone bill with only one call returns that one phone bill
+     * Tests that dumping a phone bill with only one call returns that one phone bill.
      */
     @Test
     public void testDumpToSinglePhoneCall() {
@@ -40,7 +51,7 @@ public class TextDumperTest {
     }
 
     /**
-     * Tests that dumping multiple calls is the same as the toString
+     * Tests that dumping multiple calls is the same as the toString.
      */
     @Test
     public void testDumpToMultiplePhoneCalls() {
@@ -64,5 +75,55 @@ public class TextDumperTest {
 
         dumper.dumpTo(bill, outputStream);
         assertEquals(bill.toString() + "\n" + calls, new String(out.toByteArray()));
+    }
+
+    /**
+     * Tests that creating a {@link TextDumper} with a filename has it writing to that file.
+     */
+    @Test
+    public void testDumpEmptyFile() {
+        try {
+            File emptyFile = folder.newFile();
+
+            var bill = new PhoneBill("name");
+            var dumper = new TextDumper(emptyFile.getName());
+
+            dumper.dump(bill);
+
+            assertEquals(bill.toString(), String.join("\n", Files.readAllLines(emptyFile.toPath(), Charset.defaultCharset())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Tests that if a file already exists {@link TextDumper} throws an exception.
+     */
+    @Test
+    public void testDumpExistingFile() {
+        try {
+            File emptyFile = folder.newFile();
+
+            var bill = new PhoneBill("name");
+
+            try {
+                bill.addPhoneCall(new PhoneCall("503-333-3333", "503-333-3333", "2/2/2 00:00", "2/2/3 0:00"));
+                bill.addPhoneCall(new PhoneCall("503-333-3333", "503-333-3333", "2/2/2 00:00", "2/2/3 0:00"));
+                bill.addPhoneCall(new PhoneCall("503-333-3333", "503-333-3333", "2/2/2 00:00", "2/2/3 0:00"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                fail(e.getMessage());
+            }
+
+            var dumper = new TextDumper(emptyFile.getName());
+
+            dumper.dump(bill);
+
+            thrown.expect(Exception.class);
+
+            dumper.dump(bill);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
