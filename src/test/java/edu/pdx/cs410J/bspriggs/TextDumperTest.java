1b0707e932831ce0b7d49cf58ede2fa9fe5ab05c
bspriggs <ben@sprico.com>
2018-07-11 22:54:42 -0700

    Add basic non-compiling tests

--- src/test/java/edu/pdx/cs410J/bspriggs/TextDumperTest.java
+++ src/test/java/edu/pdx/cs410J/bspriggs/TextDumperTest.java
@@ -2,9 +2,37 @@ package edu.pdx.cs410J.bspriggs;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class TextDumperTest {
 
+    /**
+     * Tests that dumping an empty phone bill returns nothing
+     */
     @Test
-    public void dump() {
+    public void testDumpToEmptyPhoneBill() {
+        var dumper = new TextDumper();
+        var out = new ByteArrayOutputStream();
+        var outputStream = new DataOutputStream(out);
+
+        dumper.dumpTo(outputStream);
+        assertEquals("", new String(out.toByteArray()));
+    }
+
+    /**
+     * Tests that dumping a phone bill with only one call returns that one phone bill
+     */
+    @Test
+    public void testDumpToSinglePhoneCall() {
+        var bill = new PhoneBill();
+        var dumper = new TextDumper(bill);
+        var out = new ByteArrayOutputStream();
+        var outputStream = new DataOutputStream(out);
+
+        dumper.dumpTo(outputStream);
+        assertEquals(bill.toString(), new String(out.toByteArray()));
     }
 }
