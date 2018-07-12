0ee053e5e55c3834eb93d7c16f54309e2348a9e3
bspriggs <ben@sprico.com>
2018-07-12 11:48:23 -0700

    Add serialize method and delimiter for later use

--- src/main/java/edu/pdx/cs410J/bspriggs/TextDumper.java
+++ src/main/java/edu/pdx/cs410J/bspriggs/TextDumper.java
@@ -1,11 +1,16 @@
 package edu.pdx.cs410J.bspriggs;
 
 import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
 
 import java.io.DataOutputStream;
 
 public class TextDumper implements edu.pdx.cs410J.PhoneBillDumper {
     private final String fileName;
+    /**
+     * Delimiter used to separate fields in a {@link PhoneCall}. See {@link TextDumper::serizlize}.
+     */
+    private static String DELIMITER = "|";
 
     public TextDumper(String fileName) {
         this.fileName = fileName;
@@ -13,6 +18,16 @@ public class TextDumper implements edu.pdx.cs410J.PhoneBillDumper {
 
     public TextDumper() {
         this.fileName = "";
+    }
+
+    /**
+     * Serializes a single {@link PhoneCall}.
+     *
+     * @param call
+     * @return A serialized {@link PhoneCall}
+     */
+    public static String serialize(AbstractPhoneCall call) {
+        return String.join(TextDumper.DELIMITER, call.getCaller(), call.getCallee(), call.getStartTimeString(), call.getEndTimeString());
     }
 
     public void dumpTo(AbstractPhoneBill phoneBill, DataOutputStream dataOutputStream) {
