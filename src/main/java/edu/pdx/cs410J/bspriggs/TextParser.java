b38f117e6955365ff290d3df70346f9133729a3f
bspriggs <ben@sprico.com>
2018-07-12 23:23:05 -0700

    Update TextParser with working implementation

--- src/main/java/edu/pdx/cs410J/bspriggs/TextParser.java
+++ src/main/java/edu/pdx/cs410J/bspriggs/TextParser.java
@@ -1,9 +1,15 @@
 package edu.pdx.cs410J.bspriggs;
 
 import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
+import edu.pdx.cs410J.ParserException;
 
-import java.io.Reader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.regex.Pattern;
 
 public class TextParser implements edu.pdx.cs410J.PhoneBillParser {
     private final Path path;
@@ -12,12 +18,39 @@ public class TextParser implements edu.pdx.cs410J.PhoneBillParser {
         this.path = path;
     }
 
-    public AbstractPhoneBill parseFrom(Reader in) {
-        return null;
-    }
-
     @Override
-    public AbstractPhoneBill parse() {
-        return null;
+    public AbstractPhoneBill<AbstractPhoneCall> parse() throws ParserException {
+        PhoneBill phoneBill = null;
+
+        try {
+            var lines = Files.readAllLines(this.path, Charset.defaultCharset());
+
+            if (lines.size() == 0) {
+                throw new ParserException("Malformatted PhoneBill, missing customer name");
+            }
+
+            var customer = lines.remove(0);
+
+            phoneBill = new PhoneBill(customer);
+            for (String line : lines) {
+                var fields = line.split(Pattern.quote(TextDumper.DELIMITER));
+
+                if (fields.length != 4) {
+                    throw new ParserException(String.format("Line was incorrectly formatted: '%s'", line));
+                }
+
+                var call = new PhoneCall(fields[0], fields[1], fields[2], fields[3]);
+
+                phoneBill.addPhoneCall(call);
+            }
+        } catch (FileNotFoundException e) {
+            throw new ParserException(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new ParserException(e.getMessage());
+        }
+
+        return phoneBill;
     }
 }
