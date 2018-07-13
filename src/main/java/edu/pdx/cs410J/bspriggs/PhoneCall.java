e254c9391702a02381ec358ce4ce056f71089e98
bspriggs <ben@sprico.com>
2018-07-13 16:10:12 -0700

    Use date instead of string

--- src/main/java/edu/pdx/cs410J/bspriggs/PhoneCall.java
+++ src/main/java/edu/pdx/cs410J/bspriggs/PhoneCall.java
@@ -4,21 +4,22 @@ import edu.pdx.cs410J.AbstractPhoneCall;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Pattern;
 
 public class PhoneCall extends AbstractPhoneCall {
-  private final String caller;
-  private final String callee;
-  private final String startTime;
-  private final String endTime;
-    private static final Pattern phoneNumberPattern = Pattern.compile("\\d\\d\\d-\\d\\d\\d-\\d\\d\\d\\d");
+    private final String caller;
+    private final String callee;
+    private final Date startTime;
+    private final Date endTime;
+    private static final Pattern phoneNumberPattern = Pattern.compile("\\d{3}-\\d{3}-\\d{4}");
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("mm/dd/yyyy hh:mm");
 
-    PhoneCall(String caller, String callee, String startDateAndTime, String endDateAndTime) throws Exception {
+    PhoneCall(String caller, String callee, String startDateAndTime, String endDateAndTime) throws ParseException {
         this.caller = validatePhoneNumber(caller);
         this.callee = validatePhoneNumber(callee);
-        this.startTime = validateDate(startDateAndTime);
-        this.endTime = validateDate(endDateAndTime);
+        this.startTime = dateFormat.parse(startDateAndTime);
+        this.endTime = dateFormat.parse(endDateAndTime);
     }
 
     private String validatePhoneNumber(String in) throws ParseException {
@@ -39,23 +40,33 @@ public class PhoneCall extends AbstractPhoneCall {
         return in;
     }
 
-  @Override
-  public String getCaller() {
-    return this.caller;
-  }
+    @Override
+    public String getCaller() {
+        return this.caller;
+    }
 
-  @Override
-  public String getCallee() {
-    return this.callee;
-  }
+    @Override
+    public String getCallee() {
+        return this.callee;
+    }
 
-  @Override
-  public String getStartTimeString() {
-    return this.startTime;
-  }
+    @Override
+    public Date getStartTime() {
+        return super.getStartTime();
+    }
 
-  @Override
-  public String getEndTimeString() {
-    return this.endTime;
-  }
+    @Override
+    public String getStartTimeString() {
+        return dateFormat.format(startTime);
+    }
+
+    @Override
+    public Date getEndTime() {
+        return super.getEndTime();
+    }
+
+    @Override
+    public String getEndTimeString() {
+        return dateFormat.format(endTime);
+    }
 }
