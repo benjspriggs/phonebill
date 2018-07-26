bc3b146a7b95a2d57e8b356d7460f7dd2f29464d
bspriggs <ben@sprico.com>
2018-07-25 23:01:56 -0700

    Remove some vestigal code

--- src/main/java/edu/pdx/cs410J/bspriggs/Project1.java
+++ src/main/java/edu/pdx/cs410J/bspriggs/Project1.java
@@ -3,67 +3,11 @@ package edu.pdx.cs410J.bspriggs;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import static java.util.Map.entry;
 
 /**
  * The main class for the CS410J Phone Bill Project
  */
 public class Project1 extends MainClassSkeleton<PhoneBill> {
-    public static final List<Map.Entry<String, String>> ARGUMENTS = Arrays.asList(
-            entry("customer", "Person whose phone bill we’re modeling"),
-            entry("callerNumber", "Phone number of caller"),
-            entry("calleeNumber", "Phone number of person who was called"),
-            entry("startTime", "Date and time call began (24-hour time)"),
-            entry("endTime", "Date and time call ended (24-hour time)")
-    );
-
-    public static final List<Map.Entry<String, String>> OPTIONS = Arrays.asList(
-            entry("-print", "Prints a description of the new phone call"),
-            entry("-README", "Prints a README for this project and exits")
-    );
-
-    protected static String build(List<Map.Entry<String, String>> f) {
-        var b = new StringBuilder();
-
-        for (Map.Entry<String, String> pair : f) {
-            b.append(String.format("  %-10s\t%s", pair.getKey(), pair.getValue()));
-            b.append(System.lineSeparator());
-        }
-
-        return b.toString();
-    }
-
-    protected static String usage() {
-        return "usage: java edu.pdx.cs410J.bspriggs.Project1 [options] <args>\n" +
-                "args are (in this order):\n" +
-                build(ARGUMENTS) +
-                "options are (options may appear in any order):\n" +
-                build(OPTIONS) +
-                "Date and time should be in the format: " + PhoneCall.DATE_FORMAT_STRING;
-    }
-
-    public static void validateEmptyArguments(String[] args) {
-        if (args == null || args.length == 0) {
-            System.err.println("Missing command line arguments");
-            System.err.println(usage());
-            System.exit(1);
-        }
-    }
-
-    protected static PhoneBill doMain(int ptr, boolean print, String[] args) {
-        try {
-            return new Project1().wrapWork(args);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            System.err.println(usage());
-            System.exit(1);
-        }
-
-        return null;
-    }
-
     @Override
     List<Argument> getArguments() {
         return Arrays.asList(
--- src/main/java/edu/pdx/cs410J/bspriggs/Project2.java
+++ src/main/java/edu/pdx/cs410J/bspriggs/Project2.java
@@ -6,70 +6,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-
-import static java.util.Map.entry;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class Project2 extends Project1 {
-    private static final List<Map.Entry<String, String>> OPTIONS = Arrays.asList(
-            entry("-print", "Prints a description of the new phone call"),
-            entry("-README", "Prints a README for this project and exits"),
-            entry("-textFile file", "Where to read/write the phone bill")
-    );
-
-    protected static String usage() {
-        return "usage: java edu.pdx.cs410J.bspriggs.Project2 [options] <args>\n" +
-                "args are (in this order):\n" +
-                Project1.build(ARGUMENTS) +
-                "options are (options may appear in any order):\n" +
-                Project1.build(OPTIONS) +
-                "Date and time should be in the format: mm/dd/yyyy hh:mm";
-    }
-
-    protected static PhoneBill doMain(int ptr, boolean print, String filename, String[] args) {
-        try {
-            return new Project2().wrapWork(args);
-        } catch (Exception e) {
-            System.err.println(e);
-            System.err.println(usage());
-            System.exit(1);
-        }
-
-        return null;
-    }
-
     public static void main(String[] args) {
-        validateEmptyArguments(args);
-
-        int ptr = 0;
-        boolean print = false;
-        String filename = null;
-
-        // parse options
-        // (this is probably better done with the apache commons-cli)
-        for (; (ptr < args.length) && (args[ptr].equals("-print") || args[ptr].equals("-README") || args[ptr].equals("-textFile")); ++ptr) {
-            switch (args[ptr]) {
-                case "-print":
-                    print = true;
-                    break;
-                case "-README":
-                    System.out.println("Optionally reads a PhoneBill " +
-                            "from a text file, " +
-                            "creates a new PhoneCall as specified on the command line, " +
-                            "adds the PhoneCall to the PhoneBill, " +
-                            "and then optionally writes the PhoneBill back to the text file.");
-                    System.out.println(usage());
-                    System.exit(0);
-                case "-textFile":
-                    // advance to filename
-                    ptr++;
-                    // get filename
-                    filename = args[ptr];
-            }
-        }
-
-        doMain(ptr, print, filename, args);
-        System.exit(0);
+        wrapMain(new Project2(), args);
     }
 
     @Override
